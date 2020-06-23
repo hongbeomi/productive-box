d90682b5064b97b6a4e85692c672a6377280f3a0
hongbeomi <dks3963@naver.com>
2020-06-23 16:00:31 +0900

    멘트 변경

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ interface IRepo {
     files: {
       [filename]: {
         // eslint-disable-next-line quotes
-        filename: (morning + daytime) > (evening + night) ? "I'm an early 🐤" : "I'm a night 🦉",
+        filename: (morning + daytime) > (evening + night) ? "I'm a morning person ☀️" : "I'm a night person🌙",
         content: lines.join('\n'),
       },
     },
